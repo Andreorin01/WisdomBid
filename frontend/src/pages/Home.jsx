d91733ca989359f13c5ecfbd35bf1be8a2800269
Ionut Artene <ionut.artene@gmail.com>
2025-05-27 12:49:00 +0300

    modified:   frontend/src/pages/Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import Button from '@mui/material/Button';
+
+function MyComponent() {
+  return (
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>
+  );
+}
+
+export default MyComponent;
 
 const Home = () => {
   return (
